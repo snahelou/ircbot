b363ac0f4f7ab8e8f6fc7166aad4962bf04c5580
root <root@ip-172-31-36-21.eu-west-1.compute.internal>
2016-08-02 12:09:57 +0000

    Notify with Register/Leave into channel

--- simple.py
+++ simple.py
@@ -1,4 +1,6 @@
 import sys
+import json
+
 import socket
 import string
 import time 
@@ -7,10 +9,16 @@ import select
 import os
 from multiprocessing import Process, Pipe
 
-HOST="54.171.171.138"
+HOST="your irc host"
 PORT=6667
 
-channel_list=['#btwin','#citadelle','#euratec','#campus','#heron','#seb','#pierre']
+channel_list=['#btwin','#citadelle','#euratec','#heron']
+register_list={
+  '#btwin'     : [],
+  '#citadelle' : [],
+  '#euratec'   : [],
+  '#heron'     : [],
+}
 
 NICK="Sacha"
 IDENT="sacha"
@@ -18,6 +26,7 @@ REALNAME="SachaBot"
 
 def process_connection(s):
   readbuffer=""
+  BOOT=0
 
   while 1:
     try:
@@ -32,6 +41,45 @@ def process_connection(s):
 
        if(line[0]=="PING"):
          s.send("PONG %s\r\n" % line[1])
+
+       file='/tmp/notify'
+       if BOOT == 0:
+         BOOT = 1
+         with open(file, 'r') as fi:
+           try:
+             register_list = json.load(fi)
+             # if the file is empty the ValueError will be thrown
+           except ValueError:
+            register_list = {}
+
+       if ( len(line) > 3 ) and (line[3][1:] == "Register"):
+         register_list[line[2]].append(line[0].split('!')[0][1:])
+         print register_list
+         with open(file, 'w') as fi:
+             data = json.dump(register_list,fi)
+         
+       if ( len(line) > 3 ) and (line[3][1:] == "Leave"):
+         register_list[line[2]].remove(line[0].split('!')[0][1:])
+         print register_list
+         with open(file, 'w') as fi:
+             data = json.dump(register_list,fi)
+
+       if ( len(line) > 3 ) and (line[1] == "QUIT"):
+         #register_list[line[2]].remove(line[0].split('!')[0][1:])
+         for a in register_list.itervalues():
+           try:
+             a.remove(line[0].split('!')[0][1:])
+           except ValueError:
+             pass
+         print register_list
+         with open(file, 'w') as fi:
+             data = json.dump(register_list,fi)
+
+       if ( len(line) > 3 ) and (line[1] == "PART"):
+         register_list[line[2]].remove(line[0].split('!')[0][1:])
+         print register_list
+         with open(file, 'w') as fi:
+             data = json.dump(register_list,fi)
     except socket.error:
       # Permet d avoir un recv non bloquant, sinon on doit attendre un message....
       print "No response from server"
@@ -47,15 +95,26 @@ def process_channel(channel,s):
   p.register(f.stdout)
 
   s.send("JOIN "+channel +"\r\n")
-
+  file = '/tmp/notify'
+  with open(file, 'r') as fi:
+    try:
+        register_list = json.load(fi)
+    # if the file is empty the ValueError will be thrown
+    except ValueError:
+        register_list = {}
   while 1:
     if p.poll(1):
       # New line type "Pop fantominus a http://www.google.fr;SebAndro;xxxx"
       # message;user1;user2
       txt = f.stdout.readline().split(';')
       message=txt[0] 
+      print txt 
       s.send('PRIVMSG ' + channel + ' : ' + message +'\r\n')
       txt.pop(0)
+      txt = txt + register_list[channel]
+      print channel
+      print register_list
+      print register_list[channel]
       for i in range(len(txt)):
         print "jenvoie a "+ txt[i].rstrip('\r\n')
         s.send('PRIVMSG ' + txt[i].rstrip('\r\n') + ' : ' + message +'\r\n')
